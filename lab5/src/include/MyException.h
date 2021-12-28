e24d2f507fa488fe4375396efdbe35b4c7bbab74
Valeriy Sinyukov <v.sinyukov@g.nsu.ru>
2021-12-28 11:48:31 +0700

    solving problems with pointer to tuple

---
+++ lab5/src/include/MyException.h
@@ -0,0 +1,39 @@
+#pragma once
+
+#include <iostream>
+
+class MyException : public exception
+{
+protected:
+	int column, line;
+public:
+	MyException(int column, int line) : column(column), line(line) {}
+	virtual ~MyException() = default;
+	virtual void errorReport() = 0;
+};
+
+class NoExcpectedData : public MyException
+{
+	string ExpectedType;
+public:
+	NoExcpectedData(int column, int line, string ExpectedType) : 
+		MyException(column,line),ExpectedType(ExpectedType)	{}
+	void errorReport()
+	{
+		cerr << "In line " << line << ", column " << column 
+			<< " no data appeared, data type expected: " << ExpectedType << '!' << endl;
+	}
+};
+
+class ExtraData: public MyException
+{
+	string extraData;
+public:
+	ExtraData(int column, int line,string extraData) :
+		MyException(column,line), extraData(extraData) {}
+	void errorReport()
+	{
+		cerr << "In line " << line << ", column " << column 
+			<< " no data expected, but " << extraData << " appeared!" << endl;
+	}
+};
--- lab5/src/include/csv_parser.h
+++ lab5/src/include/csv_parser.h
@@ -2,15 +2,13 @@
 
 #include "print_tuple.h"
 #include "MyException.h"
-#include <iostream>
 #include <fstream>
-#include <string>
 #include <sstream>
-#include <list>
+#include <string>
 
 using namespace std;
 
-template <size_t N,class Head, class ...Tail>
+template <size_t N, class Head, class ...Tail>
 struct ToTupleConverter
 {
 	static auto convert(string& str, const string& separators, const string& shielding, int line)
@@ -23,22 +21,20 @@ struct ToTupleConverter
 		istrstream >> str;
 		str.erase(0, 1);
 		auto tupleOfCurrentElement = make_tuple(t);
-		auto tuple = tuple_cat(tupleOfCurrentElement, ToTupleConverter<N + 1,Tail...>::convert(str, separators, shielding, line));
+		auto tuple = tuple_cat(tupleOfCurrentElement, ToTupleConverter<N + 1, Tail...>::convert(str, separators, shielding, line));
 		return tuple;
 	}
 };
 
-template<size_t N,class ...Tail>
-struct ToTupleConverter<N,string, Tail...>
+template<size_t N, class ...Tail>
+struct ToTupleConverter<N, string, Tail...>
 {
 	static auto convert(string& str, const string& separators, const string& shielding, int line)
 	{
 		if (str.empty())
 			throw NoExcpectedData(N, line, "string");
 		int pos = str.find_first_of(separators);
-		if (string::npos == pos)
-			pos = str.length();
-		while ((pos) && (shielding.find_first_of(str[pos + 1]) != string::npos) && (str[pos - 1] == str[pos + 1]))
+		while ((pos > 0) && (shielding.find_first_of(str[pos + 1]) != string::npos) && (str[pos - 1] == str[pos + 1]))
 		{
 			str.erase(pos + 1, 1);
 			str.erase(pos - 1, 1);
@@ -48,13 +44,20 @@ struct ToTupleConverter<N,string, Tail...>
 			pos = str.length();
 		auto tupleOfCurrentElement = make_tuple(str.substr(0, pos));
 		str.erase(0, pos + 1);
-		auto tuple = tuple_cat(tupleOfCurrentElement, ToTupleConverter<N + 1,Tail...>::convert(str, separators,shielding,line));
+		auto tuple = tuple_cat(tupleOfCurrentElement, ToTupleConverter<N + 1, Tail...>::convert(str, separators, shielding, line));
 		return tuple;
 	}
 };
 
-template <size_t N,class Head>
-struct ToTupleConverter<N,Head>
+void ExtraDataCheck(string& str, int column, int line, const string& separators)
+{
+	int nextSeparatorInd = str.find_first_of(separators);
+	if (string::npos != nextSeparatorInd)
+		throw ExtraData(column, line, str.substr(nextSeparatorInd + 1));
+}
+
+template <size_t N, class Head>
+struct ToTupleConverter<N, Head>
 {
 	static auto convert(string& str, const string& separators, const string& shielding, int line)
 	{
@@ -63,23 +66,19 @@ struct ToTupleConverter<N,Head>
 			throw NoExcpectedData(N, line, typeid(t).name());
 		istringstream istrstream(str);
 		istrstream >> t;
-		int nextSeparatorInd = str.find_first_of(separators);
-		if (string::npos != nextSeparatorInd)
-			throw ExtraData(N + 1, line, str.substr(nextSeparatorInd + 1));
+		ExtraDataCheck(str, N + 1, line, separators);
 		return make_tuple(t);
 	}
 };
 
 template <size_t N>
-struct ToTupleConverter<N,string>
+struct ToTupleConverter<N, string>
 {
 	static auto convert(string& str, const string& separators, const string& shielding, int line)
 	{
 		if (str.empty())
-			NoExcpectedData(N, line, "string");
-		int nextSeparatorInd = str.find_first_of(separators);
-		if (nextSeparatorInd != string::npos)
-			throw ExtraData(N + 1, line, str.substr(nextSeparatorInd + 1));
+			throw NoExcpectedData(N, line, "string");
+		ExtraDataCheck(str, N + 1, line, separators);
 		return make_tuple(str);
 	}
 };
@@ -87,22 +86,116 @@ struct ToTupleConverter<N,string>
 template <class ...Args>
 class CSVParser
 {
-public:
-	list<tuple<Args...>> tupleList;
-	CSVParser(ifstream& file, int skipLines, const string& separators,const string& shielding)
+	ifstream* file;
+	string separators;
+	string shielding;
+	int skipLines;
+
+	class Iterator : public iterator<input_iterator_tag, tuple<Args...>>
 	{
-		string str;
-		int i;
-		tuple<Args...> t;
-		for (i = 0; i < skipLines; ++i)
-			getline(file, str);
-		getline(file, str);
-		while (!file.eof())
+		ifstream* file = NULL;
+		string separators;
+		string shielding;
+		tuple<Args...>* currentTuple;
+		int line = 1;
+		bool endFileFlag = false;
+
+	public:
+
+		Iterator(bool endFileFlag) : endFileFlag(endFileFlag) {}
+
+		Iterator(ifstream* file, const string& separators, const string& shielding, int skipLines)
+			: file(file), separators(separators), shielding(shielding)
 		{
-			++i;
-			t = ToTupleConverter<1,Args...>::convert(str, separators, shielding,i);
-			tupleList.push_back(t);
-			getline(file, str);
+			string str;
+			getline(*file, str);
+			int i;
+			for (i = 1; i < skipLines; ++i)
+			{
+				if (file->eof())
+				{
+					endFileFlag = true;
+					return;
+				}
+				getline(*file, str);
+			}
+			currentTuple = &(ToTupleConverter<0, Args...>::convert(str, separators, shielding, line));
 		}
+
+		Iterator(const Iterator& it)
+		{
+			file = it.file;
+			separators = it.separators;
+			shielding = it.shielding;
+			line = it.line;
+			endFileFlag = it.endFileFlag;
+			currentTuple = it.currentTuple;
+		}
+
+		tuple<Args...>& operator++()
+		{
+			string str;
+			getline(*file, str);
+			++line;
+			if (file->eof())
+			{
+				endFileFlag = true;
+				currentTuple = NULL;
+			}
+			else
+				currentTuple = &(ToTupleConverter<0, Args...>::convert(str, separators, shielding, line));
+			return *currentTuple;
+		}
+
+		tuple<Args...>& operator++(int)
+		{
+			tuple<Args...> previousTuple = *currentTuple;
+			string str;
+			getline(*file, str);
+			++line;
+			if (file.eof())
+				endFileFlag = true;
+			currentTuple = &(ToTupleConverter<0, Args...>::convert(str, separators, shielding, line));
+			return previousTuple;
+		}
+
+		bool operator==(const Iterator& it) const
+		{
+			if ((this->endFileFlag) && (it.endFileFlag))
+				return true;
+			if ((this->endFileFlag) && (it.endFileFlag))
+				return true;
+			return false;
+		}
+
+		bool operator!=(const Iterator& it) const
+		{
+			return !(*this == it);
+		}
+
+		tuple<Args...>& operator*() const
+		{
+			return *currentTuple;
+		}
+
+		tuple<Args...>* operator->() const
+		{
+			return currentTuple;
+		}
+	};
+
+
+
+public:
+	CSVParser(ifstream& file, int skipLines, const string& separators, const string& shielding)
+		: file(&file), skipLines(skipLines), separators(separators), shielding(shielding) {}
+
+	Iterator begin()
+	{
+		return Iterator(file, separators, shielding, skipLines);
+	}
+	Iterator end()
+	{
+		return Iterator(true);
 	}
 };
